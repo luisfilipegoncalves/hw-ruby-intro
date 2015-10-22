b949f8a75c2977d57ed52faeed18c5351cefbb66
Luis Filipe <exnlesi@gmail.com>
2015-10-22 22:54:03 +0000

    Finished part 1 - Arrays, Hashes, and Enumerables

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -2,16 +2,30 @@
 
 # Part 1
 
+# Define a method sum(array) that takes an array of integers as an argument and
+# returns the sum of its elements. For an empty array it should return zero.
 def sum arr
-  # YOUR CODE HERE
+  return 0 if arr.empty?
+  arr.inject(:+)
 end
 
+# Define a method max_2_sum(array) which takes an array of integers as an 
+# argument and returns the sum of its two largest elements. For an empty array 
+# it should return zero. For an array with just one element, it should return
+# that element.
 def max_2_sum arr
-  # YOUR CODE HERE
+  return 0 if arr.empty?
+  return arr.first if arr.size == 1
+  arr.sort.pop(2).inject(:+)
 end
 
+# Define a method sum_to_n?(array, n) that takes an array of integers and an 
+# additional integer, n, as arguments and returns true if any two elements in 
+# the array of integers sum to n. An empty array should sum to zero by definition.
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  return false if arr.empty?
+  combinations_found = arr.combination(2).to_a.select { |o| o.inject(:+) == n }
+  combinations_found.size > 0
 end
 
 # Part 2
