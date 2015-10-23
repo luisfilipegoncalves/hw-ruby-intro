cdd25c6d8fe7aae81b4821af1d13e4b7236448af
Luis Filipe <exnlesi@gmail.com>
2015-10-23 22:07:57 +0000

    Fix part 2

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -36,12 +36,21 @@ def hello(name)
   "Hello, " + name
 end
 
+# Define a method starts_with_consonant?(s) that takes a string and returns
+# true if it starts with a consonant and false otherwise. (For our purposes, 
+# a consonant is any letter other than A, E, I, O, U.) NOTE: be sure it works 
+# for both upper and lower case and for nonletters!
 def starts_with_consonant? s
-  /^[|aeiou]/.match s
+  /^[a-zA-Z&&[^aeiouAEIOU]]/.match s
 end
 
+# Define a method binary_multiple_of_4?(s) that takes a string and returns true
+# if the string represents a binary number that is a multiple of 4. 
+# NOTE: be sure it returns false if the string is not a valid binary number!
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  value = /^[01]+$/.match s
+  return false if value.nil?
+  s.to_i(2)%4==0
 end
 
 # Part 3
